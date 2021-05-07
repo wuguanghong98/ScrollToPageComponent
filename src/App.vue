583f5f958e0b19cd0d00c96bb515b79a30e016b3
wuguanghong98 <wuguho98@163.com>
2021-05-07 16:42:39 +0800

    '上传使用方法'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--调用使用方法-->
+    <scroll-to-page arrowColor="red" liBackground="#BD362F" liOnColor="#000" :btnNum="4">
+      <div slot="1">
+        <span>
+          我是内容一
+        </span>
+        <hr>
+      </div>
+      <div slot="2">
+        <p>
+          我是内容二
+        </p>
+        <hr>
+      </div>
+      <div slot="3">
+        <h1>
+          我是内容三
+        </h1>
+        <hr>
+      </div>
+      <div slot="4">
+        <h1>
+          我是内容四
+        </h1>
+        <hr>
+      </div>
+    </scroll-to-page>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ScrollToPage from './components/ScrollToPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScrollToPage
+  },
+  data() {
+    return {
+    }
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+  body {
+    margin:0;
+    padding:0;
+  }
+  html,body{
+    width:100%;
+    height:100%;
+    overflow:hidden;
+  }
+  #app {
+    height: 100%;
+    width: 100%;
+  }
 </style>
